3c4b671f5a911336f2192b8e2e48b64057e75708
Alan Christensen <alan@christensen.org.nz>
2016-04-26 15:35:05 +1200

    turned off npm install bits as doing crazies and we include in git

--- lib/dependencies.sh
+++ lib/dependencies.sh
@@ -8,22 +8,7 @@ run_if_present() {
 }
 
 install_node_modules() {
-  local build_dir=${1:-}
-
-  if [ -e $build_dir/package.json ]; then
-    cd $build_dir
-
-    echo "Pruning any extraneous modules"
-    npm prune --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
-    if [ -e $build_dir/npm-shrinkwrap.json ]; then
-      echo "Installing node modules (package.json + shrinkwrap)"
-    else
-      echo "Installing node modules (package.json)"
-    fi
-    npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
-  else
-    echo "Skipping (no package.json)"
-  fi
+  echo "Not installing node modules as should be included in repo"
 }
 
 rebuild_node_modules() {
@@ -33,11 +18,6 @@ rebuild_node_modules() {
     cd $build_dir
     echo "Rebuilding any native modules"
     npm rebuild 2>&1
-    if [ -e $build_dir/npm-shrinkwrap.json ]; then
-      echo "Installing any new modules (package.json + shrinkwrap)"
-    else
-      echo "Installing any new modules (package.json)"
-    fi
     npm install --unsafe-perm --userconfig $build_dir/.npmrc 2>&1
   else
     echo "Skipping (no package.json)"
